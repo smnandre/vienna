2c0b593409f9d0b5fff18d1650d377c9cec9cb18
Simon André <smn.andre@gmail.com>
2024-12-03 11:15:10 +0100

    Clean templates & add nav

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -11,8 +11,6 @@ class IndexController extends AbstractController
     #[Route('/', name: 'app_index')]
     public function index(): Response
     {
-        return $this->render('index/index.html.twig', [
-            'controller_name' => 'IndexController',
-        ]);
+        return $this->render('index/index.html.twig');
     }
 }
